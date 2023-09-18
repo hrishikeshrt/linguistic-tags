198c727fd90b715a61eafdddd02197703d77895e
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2023-09-18 14:40:26 +0530

    feat: add reverseproxied and some other flask extensions

--- viewer/server.py
+++ viewer/server.py
@@ -9,10 +9,69 @@ Linguistic Tag Viewer
 ###############################################################################
 
 from flask import Flask, render_template
+from sqlalchemy import or_, and_, func
+
+from flask_admin import Admin, helpers as admin_helpers
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+from flask_wtf import CSRFProtect
+
+# local
+# from models_sqla import (db,)
+# from models_admin import (SecureAdminIndexView,)
+
+# from settings import app
+from reverseproxied import ReverseProxied
 
 ###############################################################################
 
 webapp = Flask("Linguistic Tag Viewer")
+webapp.wsgi_app = ReverseProxied(webapp.wsgi_app)
+webapp.url_map.strict_slashes = False
+
+# webapp.config['SECRET_KEY'] = app.secret_key
+webapp.config['JSON_AS_ASCII'] = False
+webapp.config['JSON_SORT_KEYS'] = False
+
+# # SQLAlchemy Config
+# webapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# webapp.config['SQLALCHEMY_DATABASE_URI'] = app.sqla['database_uri']
+# webapp.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+#     "pool_pre_ping": True,
+# }
+# # Flask Admin Theme
+# webapp.config["FLASK_ADMIN_SWATCH"] = "united"
+
+# # CSRF Token Expiry
+webapp.config['WTF_CSRF_TIME_LIMIT'] = None
+
+###############################################################################
+# Initialize standard Flask extensions
+
+# db.init_app(webapp)
+csrf = CSRFProtect(webapp)
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=webapp,
+    default_limits=["1800 per hour"],
+    storage_uri="memory://",
+)
+
+# flask-admin
+admin = Admin(
+    webapp,
+    name="Linguistic Tag Viewer Admin",
+    # index_view=SecureAdminIndexView(
+    #     name="Database",
+    #     url="/admin/database"
+    # ),
+    template_mode="bootstrap4",
+    # base_template="admin_base.html",
+)
+# admin.add_view(UserModelView(User, db.session))
+# admin.add_view(LabelModelView(NodeLabel, db.session, category="Ontology"))
 
 ###############################################################################
 
@@ -55,4 +114,19 @@ def show_compare_tag():
 ###############################################################################
 
 if __name__ == "__main__":
-    webapp.run(debug=True)
+    import argparse
+    import socket
+
+    hostname = socket.gethostname()
+    default_host = socket.gethostbyname(hostname)
+    default_port = '5000'
+
+    parser = argparse.ArgumentParser(description="Linguistic Tag Viewer Server")
+    parser.add_argument("-H", "--host", help="Hostname", default=default_host)
+    parser.add_argument("-P", "--port", help="Port", default=default_port)
+    args = vars(parser.parse_args())
+
+    host = args["host"]
+    port = args["port"]
+
+    webapp.run(host=host, port=port, debug=True)
